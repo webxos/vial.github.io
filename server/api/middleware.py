e05a2140aa63ffdf30f187ee672f407f483ec753
webXOS <eefeft@gmail.com>
2025-08-21 08:52:25 -0400

    Update middleware.py

--- server/api/middleware.py
+++ server/api/middleware.py
@@ -1,11 +1,10 @@
 from fastapi import Request
-from fastapi.responses import JSONResponse
-from server.logging import logger
+from fastapi.responses import Response
+
 
 async def cors_middleware(request: Request, call_next):
     response = await call_next(request)
     response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
-    response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type"
-    logger.info(f"CORS headers added for {request.url}")
+    response.headers["Access-Control-Allow-Methods"] = "GET,POST,OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Authorization,Content-Type"
     return response
