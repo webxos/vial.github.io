a11929403aa4f1bd665c4ffa559f4e7f6e952fdd
webXOS <eefeft@gmail.com>
2025-08-21 09:11:09 -0400

    Update test_redis_handler.py

--- server/tests/test_redis_handler.py
+++ server/tests/test_redis_handler.py
@@ -10,7 +10,6 @@ def test_redis_get_set(mock_redis):
     redis_handler.setex("test_key", 60, "2")
     mock_redis.return_value.setex.assert_called_with("test_key", 60, "2")
 
-
 def test_redis_incr(mock_redis):
     mock_redis.return_value.incr.return_value = 2
     result = redis_handler.incr("test_key")
