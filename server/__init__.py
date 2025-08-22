b3e1d39c275c6c06a3b283b636af44276cad1124
webXOS <eefeft@gmail.com>
2025-08-21 22:43:59 -0400

    Update __init__.py

--- server/__init__.py
+++ server/__init__.py
@@ -1,26 +1,5 @@
-from .security import verify_token, generate_credentials
-from .services.database import init_db
-from .models.mcp_alchemist import mcp_alchemist
-from .models.webxos_wallet import webxos_wallet
-from .sdk.vial_sdk import vial_sdk
-from .services.git_trainer import git_trainer
-from .services.mongodb_handler import mongodb_handler
-from .automation.auto_deploy import auto_deploy
-from .automation.auto_scheduler import auto_scheduler
-from .quantum.quantum_sync import quantum_sync
-from .logging import logger
+from .mcp_server import app
+from .api import auth, endpoints, quantum_endpoints, websocket, copilot_integration, jsonrpc, void, troubleshoot, help, comms_hub, upload, stream
+from .services import agent_tasks, prompt_training, training_scheduler, advanced_logging, error_recovery
 
-__all__ = [
-    "verify_token",
-    "generate_credentials",
-    "init_db",
-    "mcp_alchemist",
-    "webxos_wallet",
-    "vial_sdk",
-    "git_trainer",
-    "mongodb_handler",
-    "auto_deploy",
-    "auto_scheduler",
-    "quantum_sync",
-    "logger"
-]
+__all__ = ["app", "auth", "endpoints", "quantum_endpoints", "websocket", "copilot_integration", "jsonrpc", "void", "troubleshoot", "help", "comms_hub", "upload", "stream", "agent_tasks", "prompt_training", "training_scheduler", "advanced_logging", "error_recovery"]
