9d788c09ecb30391d41a74d09f1f6ddb4a779130
webXOS <eefeft@gmail.com>
2025-08-22 09:44:49 -0400

    Update __init__.py

--- server/__init__.py
+++ server/__init__.py
@@ -1,5 +1,6 @@
 from .mcp_server import app
-from .api import auth, endpoints, quantum_endpoints, websocket, copilot_integration, jsonrpc, void, troubleshoot, help, comms_hub, upload, stream
-from .services import agent_tasks, prompt_training, training_scheduler, advanced_logging, error_recovery
+from .api import auth, endpoints, quantum_endpoints, websocket
+from .services import vial_manager, database, prompt_training
 
-__all__ = ["app", "auth", "endpoints", "quantum_endpoints", "websocket", "copilot_integration", "jsonrpc", "void", "troubleshoot", "help", "comms_hub", "upload", "stream", "agent_tasks", "prompt_training", "training_scheduler", "advanced_logging", "error_recovery"]
+__all__ = ['app', 'auth', 'endpoints', 'quantum_endpoints', 'websocket',
+           'vial_manager', 'database', 'prompt_training']
