a9b585cfe80e517a660607614a4c1afd266330de
webXOS <eefeft@gmail.com>
2025-08-22 00:54:12 -0400

    Delete server/tests/integration_testsuite.py

--- server/tests/integration_testsuite.py
+++
@@ -1,8 +0,0 @@
-from fastapi.testclient import TestClient
-from server.mcp_server import app
-
-
-def test_integration():
-    client = TestClient(app)
-    resp = client.post("/quantum/sync", json={"vial_id": "vial1"})
-    assert resp.status_code == 200
