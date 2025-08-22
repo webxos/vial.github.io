f474730cbb824e58333dac39e21b715ac06808b7
webXOS <eefeft@gmail.com>
2025-08-22 10:11:44 -0400

    Update advanced_logging.py

--- server/services/advanced_logging.py
+++ server/services/advanced_logging.py
@@ -1,8 +1,10 @@
 import logging
 
+
 logging.basicConfig(level=logging.INFO,
                    format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
+
 
 class AdvancedLogger:
     def log(self, message):
