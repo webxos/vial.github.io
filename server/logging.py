ce88d48ec885ad53ae7c56d2da49d5f5a628edda
webXOS <eefeft@gmail.com>
2025-08-22 11:11:19 -0400

    Update logging.py

--- server/logging.py
+++ server/logging.py
@@ -1,11 +1,13 @@
 import logging
+import json
 
 
 logging.basicConfig(level=logging.INFO,
-                   format='%(asctime)s - %(levelname)s - %(message)s')
+                    format='%(asctime)s - %(levelname)s - %(message)s - %(extra)s')
 logger = logging.getLogger(__name__)
 
 
 class LogHandler:
-    def log(self, message, extra=None):
-        logger.info(message, extra=extra or {})
+    def log(self, message: str, extra: dict = None):
+        extra = extra or {}
+        logger.info(message, extra={'extra': json.dumps(extra)})
