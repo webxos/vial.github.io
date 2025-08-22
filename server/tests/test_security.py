9e26564d4ceef5a0542639affdbe6865a82cfc9a
webXOS <eefeft@gmail.com>
2025-08-22 10:02:36 -0400

    Update test_security.py

--- server/tests/test_security.py
+++ server/tests/test_security.py
@@ -1,12 +1,12 @@
 from fastapi.testclient import TestClient
 from server.mcp_server import app
 
-# ... (previous content assumed)
 
 def test_security_headers():
     client = TestClient(app)
     resp = client.get("/health")
     assert "X-Content-Type-Options" in resp.headers
+
 
 def test_oauth():
     client = TestClient(app)
