25647b8de49667186a7225d17fd22d7c1733a7e2
webXOS <eefeft@gmail.com>
2025-08-22 18:02:14 -0400

    Update __init__.py

--- server/__init__.py
+++ server/__init__.py
@@ -1,6 +1,1 @@
-from .mcp_server import app
-from .api import auth, endpoints, quantum_endpoints, websocket
-from .services import vial_manager, database, prompt_training
-
-__all__ = ['app', 'auth', 'endpoints', 'quantum_endpoints', 'websocket',
-           'vial_manager', 'database', 'prompt_training']
+# Empty file to make server a Python package
