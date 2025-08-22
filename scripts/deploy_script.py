82089a9d8b005e3e5cd0bb86f2054910d7b987a1
webXOS <eefeft@gmail.com>
2025-08-22 09:44:11 -0400

    Update deploy_script.py

--- scripts/deploy_script.py
+++ scripts/deploy_script.py
@@ -1,8 +1,14 @@
+
 import subprocess
+
+
 def deploy():
+    subprocess.run(["docker-compose", "down"])
     subprocess.run(["docker-compose", "up", "--build", "-d"])
     subprocess.run(["git", "add", "."])
     subprocess.run(["git", "commit", "-m", "Final deploy"])
     subprocess.run(["git", "push", "origin", "main"])
+
+
 if __name__ == "__main__":
     deploy()
