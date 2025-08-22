1391dca4cb3e2608fa756ec9223566644bc87fde
webXOS <eefeft@gmail.com>
2025-08-22 10:56:35 -0400

    Update quantum_sync.py

--- server/quantum/quantum_sync.py
+++ server/quantum/quantum_sync.py
@@ -1,7 +1,13 @@
 from qiskit import QuantumCircuit, Aer, execute
+from pydantic import BaseModel
 
 
-class QuantumSync:
+class Component(BaseModel):
+    id: str
+    type: str
+
+
+class QuantumVisualSync:
     def __init__(self, vial_id):
         self.vial_id = vial_id
     
@@ -12,3 +18,10 @@ class QuantumSync:
         backend = Aer.get_backend('qasm_simulator')
         result = execute(qc, backend, shots=1).result()
         return {"vial_id": vial_id, "quantum_state": str(result.get_counts())}
+    
+    def create_quantum_circuit_from_visual(self, components: list[Component]):
+        qc = QuantumCircuit(len(components), len(components))
+        for comp in components:
+            if comp.type == "quantum_gate":
+                qc.h(0)
+        return {"circuit": str(qc), "visualization": {"nodes": [comp.id for comp in components]}}
