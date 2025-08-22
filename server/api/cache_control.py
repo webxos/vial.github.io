d0c00a398b9c3e749925377ca95327efc3ff908a
webXOS <eefeft@gmail.com>
2025-08-22 11:51:14 -0400

    Update cache_control.py

--- server/api/cache_control.py
+++ server/api/cache_control.py
@@ -1,5 +1,20 @@
+from fastapi import Request
+from server.services.advanced_logging import AdvancedLogger
+import time
+import hashlib
 
-async def cache_response(request, call_next):
+
+logger = AdvancedLogger()
+cache = {}
+
+
+async def cache_response(request: Request, call_next):
+    cache_key = hashlib.md5(str(request.url).encode()).hexdigest()
+    if cache_key in cache and time.time() - cache[cache_key]["timestamp"] < 300:
+        logger.log("Cache hit", extra={"url": str(request.url)})
+        return cache[cache_key]["response"]
+    
     response = await call_next(request)
-    response.headers["Cache-Control"] = "public, max-age=3600"
+    cache[cache_key] = {"response": response, "timestamp": time.time()}
+    logger.log("Cache miss, stored response", extra={"url": str(request.url)})
     return response
