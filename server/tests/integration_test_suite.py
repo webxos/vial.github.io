354fb6360cb69fb0f0d266164cf4e60cd5a171af
webXOS <eefeft@gmail.com>
2025-08-22 00:54:21 -0400

    Update integration_test_suite.py

--- server/tests/integration_test_suite.py
+++ server/tests/integration_test_suite.py
@@ -1,5 +1,7 @@
 from fastapi.testclient import TestClient
 from server.mcp_server import app
+
+
 def test_integration():
     client = TestClient(app)
     resp = client.post("/quantum/sync", json={"vial_id": "vial1"})
