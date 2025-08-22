8a3b20ccb53dfbd8ef287805150d9ac305f2f600
webXOS <eefeft@gmail.com>
2025-08-22 09:55:02 -0400

    Update webxos_wallet.py

--- server/models/webxos_wallet.py
+++ server/models/webxos_wallet.py
@@ -1,16 +1,12 @@
-from sqlalchemy import Column, String, Float
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+
 class WalletModel(Base):
     __tablename__ = "wallets"
-
-    user_id = Column(String, primary_key=True)
-    balance = Column(Float, nullable=False)
-    network_id = Column(String, nullable=False)
-
-    def dict(self):
-        return {"user_id": self.user_id, "balance": self.balance, "network_id": self.network_id}
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(String, unique=True, index=True)
+    balance = Column(Integer)
+    network_id = Column(String)
