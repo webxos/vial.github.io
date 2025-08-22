be012743a853cf0dca8f3288d5013ad59c5a5025
webXOS <eefeft@gmail.com>
2025-08-22 09:57:56 -0400

    Update security_headers.py

--- server/security/security_headers.py
+++ server/security/security_headers.py
@@ -1,10 +1,6 @@
 from fastapi import FastAPI
-from fastapi.security import OAuth2PasswordBearer
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-def setup_security_headers(app: FastAPI):
-    @app.middleware("http")
-    async def add_security_headers(request, call_next):
-        response = await call_next(request)
-        response.headers["X-Content-Type-Options"] = "nosniff"
-        response.headers["X-Frame-Options"] = "DENY"
-        return response
+
+async def add_security_headers(request, call_next):
+    response = await call_next(request)
+    response.headers["X-Frame-Options"] = "DENY"
+    return response
