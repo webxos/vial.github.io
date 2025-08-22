a110f22ca2acdb9eb2693a9625cd6f4f36623146
webXOS <eefeft@gmail.com>
2025-08-22 16:10:00 -0400

    Update test_terminal_commands.py

--- server/tests/test_terminal_commands.py
+++ server/tests/test_terminal_commands.py
@@ -4,6 +4,7 @@ from server.utils.terminal_commands import execute_terminal_command
 from server.services.database import SessionLocal
 from server.models.webxos_wallet import Wallet
 
+
 @pytest.mark.asyncio
 async def test_terminal_commands():
     """Test terminal command execution with reputation check."""
