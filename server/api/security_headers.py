b20fc04d53ac56060984e89fb88c5ff95ae73a5c
webXOS <eefeft@gmail.com>
2025-08-21 21:46:28 -0400

    Update security_headers.py

--- server/api/security_headers.py
+++ server/api/security_headers.py
@@ -1,22 +1,17 @@
-from fastapi import FastAPI
-from starlette.middleware.base import BaseHTTPMiddleware
-from starlette.requests import Request
-from server.config import settings
+from fastapi import Request
+from server.api.security import SecurityManager
 
-class SecurityHeadersMiddleware(BaseHTTPMiddleware):
-    async def dispatch(self, request: Request, call_next):
+
+class SecurityHeaders:
+    def __init__(self):
+        self.security = SecurityManager()
+
+    async def add_headers(self, request: Request, call_next):
         response = await call_next(request)
-        response.headers["X-Content-Type-Options"] = "nosniff"
-        response.headers["X-Frame-Options"] = "DENY"
-        response.headers["Content-Security-Policy"] = (
-            "default-src 'self'; "
-            "script-src 'self' https://vial.github.io; "
-            "style-src 'self' https://vial.github.io; "
-            "connect-src 'self' ws://localhost:8000 https://vial.github.io"
-        )
-        response.headers["Strict-Transport-Security"] = "max-age=31536000; includeSubDomains"
-        response.headers["X-XSS-Protection"] = "1; mode=block"
+        headers = self.security.get_security_headers()
+        for key, value in headers.items():
+            response.headers[key] = value
         return response
 
-def setup_security_headers(app: FastAPI):
-    app.add_middleware(SecurityHeadersMiddleware)
+
+security_headers = SecurityHeaders()
