951b1005996b1caf719b2c40ad5c058a040e234f
webXOS <eefeft@gmail.com>
2025-08-21 23:03:30 -0400

    Update webxos_wallet.py

--- server/models/webxos_wallet.py
+++ server/models/webxos_wallet.py
@@ -3,7 +3,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
-
 Base = declarative_base()
 
 class WalletModel(Base):
