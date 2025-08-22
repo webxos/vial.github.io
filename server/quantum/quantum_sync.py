f70c29eb28cefcf3eca9c9e077d05cf72dab3f4b
webXOS <eefeft@gmail.com>
2025-08-22 00:34:06 -0400

    Update quantum_sync.py

--- server/quantum/quantum_sync.py
+++ server/quantum/quantum_sync.py
@@ -1,24 +1,12 @@
-from qiskit import QuantumCircuit, execute, Aer
-from fastapi import FastAPI
-
+from qiskit import QuantumCircuit, Aer, execute
 class QuantumSync:
-    def __init__(self, app: FastAPI):
-        self.app = app
-
-    def sync_quantum_state(self, vial_id: str):
+    def __init__(self, vial_id):
+        self.vial_id = vial_id
+    
+    def sync_quantum_state(self, vial_id):
         qc = QuantumCircuit(2, 2)
         qc.h(0)
         qc.cx(0, 1)
-        backend = Aer.get_backend("qasm_simulator")
-        job = execute(qc, backend, shots=1024)
-        result = job.result()
-        counts = result.get_counts(qc)
-        return {"vial_id": vial_id, "quantum_state": counts}
-
-def setup_quantum_sync(app: FastAPI):
-    quantum_sync = QuantumSync(app)
-    app.state.quantum_sync = quantum_sync
-
-    @app.post("/quantum/sync")
-    async def sync_endpoint(vial_id: str):
-        return quantum_sync.sync_quantum_state(vial_id)
+        backend = Aer.get_backend('qasm_simulator')
+        result = execute(qc, backend, shots=1).result()
+        return {"vial_id": vial_id, "quantum_state": str(result.get_counts())}
