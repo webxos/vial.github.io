e9629af15323a0a708012fda48183d5f4830745f
webXOS <eefeft@gmail.com>
2025-08-22 09:54:28 -0400

    Update auth_agent.py

--- server/models/auth_agent.py
+++ server/models/auth_agent.py
@@ -1,8 +1,5 @@
 from pydantic import BaseModel
 
-class AuthAgent(BaseModel):
-    user_id: str
-    token: str
 
-    def process_user(self, user_data: dict):
-        return {"user_id": user_data.get("id"), "username": user_data.get("login")}
+class AuthAgent(BaseModel):
+    token: str
