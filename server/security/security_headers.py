7e3bc652e1499f4888e3dc1c597981d026507a6b
webXOS <eefeft@gmail.com>
2025-08-22 11:28:02 -0400

    Update security_headers.py

--- server/security/security_headers.py
+++ server/security/security_headers.py
@@ -1,5 +1,16 @@
+from fastapi import FastAPI
+from fastapi.responses import Response
+from server.services.advanced_logging import AdvancedLogger
 
-async def add_security_headers(request, call_next):
-    response = await call_next(request)
-    response.headers["X-Frame-Options"] = "DENY"
-    return response
+
+def setup_security_headers(app: FastAPI):
+    logger = AdvancedLogger()
+
+    @app.middleware("http")
+    async def add_security_headers(request, call_next):
+        response = await call_next(request)
+        response.headers["X-Content-Type-Options"] = "nosniff"
+        response.headers["X-Frame-Options"] = "DENY"
+        response.headers["Content-Security-Policy"] = "default-src 'self'"
+        logger.log("Security headers added", extra={"path": request.url.path})
+        return response
