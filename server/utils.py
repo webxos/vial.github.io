89b05f6706a53e349f2dae34f3aa99322d554085
webXOS <eefeft@gmail.com>
2025-08-22 00:49:07 -0400

    Update utils.py

--- server/utils.py
+++ server/utils.py
@@ -1,14 +1,9 @@
 import json
-from fastapi import HTTPException
 
-def parse_json(data: str):
-    try:
-        return json.loads(data)
-    except json.JSONDecodeError:
-        raise HTTPException(status_code=400, detail="Invalid JSON format")
 
-def validate_wallet(data: dict):
-    required_fields = ["user_id", "balance", "network_id"]
-    if not all(field in data for field in required_fields):
-        raise HTTPException(status_code=400, detail="Missing required wallet fields")
-    return data
+def parse_json(data):
+    return json.loads(data)
+
+
+def validate_wallet(data):
+    return {"user_id": data.get("user_id"), "balance": data.get("balance")}
