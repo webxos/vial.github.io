d99065bef6aeb9e1d79050c5d6873325be1837b4
webXOS <eefeft@gmail.com>
2025-08-23 01:12:12 -0400

    Update _apps.js

--- pages/_apps.js
+++ pages/_apps.js
@@ -1,21 +1,10 @@
+import { SessionProvider } from 'next-auth/react';
 import '../styles/globals.css';
-import { useEffect, useState } from 'react';
 
 export default function App({ Component, pageProps }) {
-  const [svgEditor, setSvgEditor] = useState(null);
-
-  useEffect(() => {
-    const loadSvgEditor = async () => {
-      const SVGEditor = require('next-svg-editor');
-      setSvgEditor(() => SVGEditor);
-    };
-    loadSvgEditor();
-  }, []);
-
   return (
-    <div className="app-container">
-      {svgEditor && <svgEditor.Component />}
+    <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
-    </div>
+    </SessionProvider>
   );
 }
