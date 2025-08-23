79535d42c89b33b6f530389e56077812f44f6f33
webXOS <eefeft@gmail.com>
2025-08-23 03:39:33 -0400

    Update __init__.py

--- server/__init__.py
+++ server/__init__.py
@@ -1,1 +1,19 @@
-# Empty file to make server a Python package
+from .services.agent_tasks import AgentTasks
+from .services.backup_restore import BackupRestore
+from .services.error_logging import ErrorLogger
+from .services.github_integration import GitHubIntegration
+from .services.memory_manager import MemoryManager
+from .services.notifications import NotificationService
+from .api.rate_limiter import RateLimiter
+from .logging_config import logger
+
+__all__ = [
+    "AgentTasks",
+    "BackupRestore",
+    "ErrorLogger",
+    "GitHubIntegration",
+    "MemoryManager",
+    "NotificationService",
+    "RateLimiter",
+    "logger"
+]
