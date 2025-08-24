375fb075df1c73da1cefc28ab4e24252bb33f360
webXOS <eefeft@gmail.com>
2025-08-24 01:05:06 -0400

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'circuit-lab-v1.0.0';
+const urlsToCache = [
+  '/',
+  '/index.js',
+  '/styles/laboratory.css',
+  '/typescript/dist/CircuitDesigner.js',
+  '/typescript/dist/RealTimeDiagram.js',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
