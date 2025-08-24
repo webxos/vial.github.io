4070b4f8621e151a720c910212193e6e9dfe9a77
webXOS <eefeft@gmail.com>
2025-08-24 15:33:16 -0400

    Create svg-transpiler.js

---
+++ public/js/svg-transpiler.js
@@ -0,0 +1,31 @@
+```javascript
+export default class SVGTranspiler {
+  constructor({ containerId, onError }) {
+    this.container = document.getElementById(containerId);
+    this.svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    this.svg.setAttribute("width", "100%");
+    this.svg.setAttribute("height", "200");
+    this.container.appendChild(this.svg);
+  }
+
+  transpileSVG(script, style = { fill: "neon-green", stroke: "black" }) {
+    try {
+      const parser = new DOMParser();
+      const svgDoc = parser.parseFromString(script, "image/svg+xml").documentElement;
+      this.svg.innerHTML = svgDoc.innerHTML;
+      this.applyWebXOSStyle(svgDoc, style);
+      return this.svg.outerHTML;
+    } catch (error) {
+      onError(`SVG Transpile Error: ${error.message}`);
+      return "";
+    }
+  }
+
+  applyWebXOSStyle(svgElement, style) {
+    svgElement.querySelectorAll("*").forEach(el => {
+      el.setAttribute("fill", style.fill);
+      el.setAttribute("stroke", style.stroke);
+    });
+  }
+}
+```
